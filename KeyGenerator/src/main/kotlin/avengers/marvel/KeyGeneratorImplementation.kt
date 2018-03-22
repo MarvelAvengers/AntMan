cb5145d8348567b6a55aba022e7cad36f2629208
Rishabh <rishabh.ja@flock.co>
2018-03-22 09:39:36 +0530

    use base64 encoding to generate random string

--- KeyGenerator/src/main/kotlin/avengers/marvel/KeyGeneratorImplementation.kt
+++ KeyGenerator/src/main/kotlin/avengers/marvel/KeyGeneratorImplementation.kt
@@ -1,13 +1,21 @@
 package avengers.marvel
 
 import io.grpc.stub.StreamObserver
+import java.util.Base64
+import java.util.Random
 
-class KeyGeneratorImplementation : KeyGeneratorApiGrpc.KeyGeneratorApiImplBase()
-{
+class KeyGeneratorImplementation : KeyGeneratorApiGrpc.KeyGeneratorApiImplBase() {
     override fun fetchKey(request: FetchKeyRequest?, responseObserver: StreamObserver<FetchKeyResponse>?) {
         responseObserver?.apply {
-            onNext(FetchKeyResponse.newBuilder().setKey("1").build())
+            onNext(FetchKeyResponse.newBuilder().setKey(generateKey()).build())
             onCompleted()
         }
+    }
+
+    private fun generateKey(): String {
+        val randomBytes = ByteArray(256).apply {
+            Random().nextBytes(this)
+        }
+        return Base64.getEncoder().encodeToString(randomBytes)
     }
 }
